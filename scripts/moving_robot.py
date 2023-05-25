b812c87b98f0f5b152d4d68421a4528c69295b89
root <giovannidimarco06@gmail.com>
2023-05-25 21:49:18 +0000

    camera, ik and move_base now are working fine

--- scripts/moving_robot.py
+++ scripts/moving_robot.py
@@ -1,16 +1,56 @@
-#!/usr/bin/python
-import moveit_commander
+#!/usr/bin/env python
+
 import rospy
-import roscpp
+import math
+import moveit_commander
+import tf
+import sys
+from geometry_msgs.msg import PoseStamped
 
-rospy.init_node('moving_robot')
+def rotate_end_effector():
+    # Initialize moveit_commander and rospy.
+    moveit_commander.roscpp_initialize(sys.argv)
+    rospy.init_node('rotate_end_effector_node', anonymous=True)
 
-robot = moveit_commander.RobotCommander()
-group = moveit_commander.MoveGroupCommander("arm")
+    robot = moveit_commander.RobotCommander()
 
-# Genera un target casuale
-group.set_random_target()
+    # Get the specific group for the end effector
+    group = moveit_commander.MoveGroupCommander("arm")
 
-plan = group.plan()
-group.execute(plan)
+    # Get the current pose
+    current_pose = group.get_current_pose().pose
+
+    # Convert current orientation to euler
+    euler = tf.transformations.euler_from_quaternion([
+        current_pose.orientation.x,
+        current_pose.orientation.y,
+        current_pose.orientation.z,
+        current_pose.orientation.w])
+
+    # Add 180 degrees to the current orientation
+    euler = [euler[0], euler[1], euler[2] + math.pi]
+
+    # Convert back to quaternion
+    quaternion = tf.transformations.quaternion_from_euler(*euler)
+
+    # Update the target pose
+    current_pose.orientation.x = quaternion[0]
+    current_pose.orientation.y = quaternion[1]
+    current_pose.orientation.z = quaternion[2]
+    current_pose.orientation.w = quaternion[3]
+
+    # Set the new pose as the target
+    group.set_pose_target(current_pose)
+
+    # Plan and execute
+    group.go(wait=True)
+
+    # Stop the group to ensure no residual movement
+    group.stop()
+
+if __name__ == '__main__':
+    try:
+        rotate_end_effector()
+    except rospy.ROSInterruptException:
+        pass
 
