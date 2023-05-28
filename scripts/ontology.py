406f91c1c34444344502378885a119828e67527c
root <giovannidimarco06@gmail.com>
2023-05-28 14:34:02 +0000

    now the markers detection works fine

--- scripts/ontology.py
+++ scripts/ontology.py
@@ -10,10 +10,34 @@ from std_msgs.msg import Bool
 #from assignment1 import Empty
 from std_srvs.srv import Empty
 from armor_api.armor_client import ArmorClient
+import rospy
+from assignment2.srv import ArucoDetection
+
+
+def aruco_detection_client():
+    #starting rotating   <-------------- 
+    
+
+    rospy.wait_for_service('aruco_detection')
+
+    try:
+        aruco_detection = rospy.ServiceProxy('aruco_detection', ArucoDetection)
+        response = aruco_detection()
+        rospy.loginfo("Detected IDs: %s", response.ids)
+    except rospy.ServiceException as e:
+        rospy.logerr("Service call failed: %s", e)
+
+
+
 
 def service_callback(request):
 
     rospy.wait_for_service('armor_interface_srv')
+    
+    aruco_detection_client()
+    
+    
+    
     # initialization of the map
 
     armcli = ArmorClient("example", "ontoRef")
@@ -35,7 +59,7 @@ def service_callback(request):
     armcli.call('DISJOINT','IND','',['R1','R2','R3','R4','E','C1','C2','D1','D2','D3','D4','D5','D6','D7'])
     armcli.call('REASON','','',[''])
 
-
+      
     
     armcli.manipulation.add_dataprop_to_ind("visitedAt", "R1", "Long", str(math.floor(time.time())))
     #rospy.sleep(random.uniform(minwait, maxwait))
@@ -68,7 +92,7 @@ def service_callback(request):
 
 def main():
     rospy.init_node('InitMapNode')
-    
+
     # Create the service
     service = rospy.Service('initmap_service', Empty, service_callback)
     
